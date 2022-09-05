8db66f2686a0c195e82fcfae4757fe82eb7f0ab6
Vlad <ricean.vlad7@gmail.com>
2022-09-05 09:24:52 +0300

    -finished codelab

--- app/src/main/java/com/example/android/marsphotos/network/MarsPhoto.kt
+++ app/src/main/java/com/example/android/marsphotos/network/MarsPhoto.kt
@@ -1,6 +1,8 @@
 package com.example.android.marsphotos.network
 
+import com.squareup.moshi.Json
+
 data class MarsPhoto(
     val id: String,
-    val img_src: String
+    @Json(name = "img_src") val imgSrcUrl: String
 )
